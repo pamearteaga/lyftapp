8fa0090f15e96dae7553a816f64054c995a464bb
pamearteaga <pe_arteaga@hotmail.com>
2017-12-14 02:05:11 -0300

    Formularios

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,6 +15,29 @@ function showPage() {
 
 $(document).ready(function(){
 
+   $("#phone").keydown(function(){
+   var number = $(this).val();
+   for (var i = 0; i < 1; i++){
+    if (number.length == 9){
+     $("#paso2").removeAttr("disabled")
+    }else{
+     $("#paso2").attr("disabled","disabled")
+    };
+   };
+ });
+
+   $("#insertCode").keydown(function(){
+   var number = $(this).val();
+   for (var i = 0; i < 1; i++){
+    if (number.length == 2){
+     $("#paso3").removeAttr("disabled")
+    }else{
+     $("#paso3").attr("disabled","disabled")
+    };
+   };
+ });
+
+
 /*botones next*/
   $("#paso1").click(function(){
    $('#inicio').addClass('hidden');
@@ -22,8 +45,10 @@ $(document).ready(function(){
   });
 
   $("#paso2").click(function(){
+   var number = $("#phone").val();
    $('#singup').addClass('hidden');
    $('#verify').removeClass('hidden');
+   $('#text-number').append('<p class="margin">' + 'Enter the code send to ' + number + '</p>');
   });
 
   $("#paso3").click(function(){
@@ -42,8 +67,9 @@ $(document).ready(function(){
    for (var i = 0; i < 3; i++){
     code += str.charAt(Math.floor(Math.random() * str.length));
    };
-    alert(code);
+    alert("Tu código es LAB " + code);
  });
+
 
 
 
